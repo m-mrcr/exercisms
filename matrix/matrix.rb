949e12460ded6b1032e2a7ea866cb7fa90371510
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-03-17 15:55:16 -0600

    Complete Columns Method, and Overall Challenge

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -7,4 +7,17 @@ class Matrix
   def rows
     input.split("\n").map {|n| n.split(' ').map(&:to_i)}
   end
+
+  def columns
+    count = rows[0].count
+    arrays = Array.new(count){Array.new}
+    rows.each do |row|
+        acc = 0
+      count.times do 
+        arrays[acc] << row[acc]
+        acc += 1
+      end
+    end
+    arrays
+  end
 end
