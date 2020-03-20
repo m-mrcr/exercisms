e0371c8308efc026041b7b82c8f0238cd4f4d9eb
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-03-20 15:00:33 -0600

    Add alternate solution

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,23 +1,31 @@
+# class Matrix
+#   def initialize(input)
+#       @input = input
+#   end
+
+#   def rows
+#     @input.split("\n").map {|n| n.split(' ').map(&:to_i)}
+#   end
+
+#   def columns
+#     count = rows[0].count
+#     arrays = Array.new(count){Array.new}
+#     rows.each do |row|
+#         acc = 0
+#       count.times do 
+#         arrays[acc] << row[acc]
+#         acc += 1
+#       end
+#     end
+#     arrays
+#   end
+# end
+
 class Matrix
-  attr_reader :input
-  def initialize(input)
-      @input = input
-  end
+  attr_reader :rows, :columns
 
-  def rows
-    input.split("\n").map {|n| n.split(' ').map(&:to_i)}
-  end
-
-  def columns
-    count = rows[0].count
-    arrays = Array.new(count){Array.new}
-    rows.each do |row|
-        acc = 0
-      count.times do 
-        arrays[acc] << row[acc]
-        acc += 1
-      end
-    end
-    arrays
+  def initialize(rep)
+    @rows = rep.each_line.map { |x| x.split.map(&:to_i) }
+    @columns = @rows.transpose
   end
 end
