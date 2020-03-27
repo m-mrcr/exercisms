ed35d3222aca310b41d50cd7a3f64b7eda01364f
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-03-27 12:01:43 -0600

    Refactor Resistor Color Duo Challenge

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -2,6 +2,6 @@ class ResistorColorDuo
   RESISTORS = %w[black brown red orange yellow green blue violet grey white].zip(0..).to_h
   
    def self.value(colors)
-    colors.first(2).map {|color| RESISTORS[color].to_s}.join.to_i
+    colors.first(2).map {|color| RESISTORS[color]}.join.to_i
   end
 end
