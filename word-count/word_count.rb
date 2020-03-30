8a591a4d2255ebde38dbd431b5ce18c841cf503c
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-03-30 10:03:22 -0600

    Complete Word Count Challenge

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,8 +1,9 @@
-=begin
-Write your code for the 'Word Count' exercise in this file. Make the tests in
-`word_count_test.rb` pass.
+class Phrase
+  def initialize(input)
+    @input = input.downcase.scan(/\b[\w']+\b/)
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/word-count` directory.
-=end
-
+  def word_count
+    counts = @input.each_with_object(Hash.new(0)) {|word, counts| counts[word] += 1}
+  end
+end
