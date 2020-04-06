dfab03ff9c8684a657c47f3d0599da6aeb15ea17
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-04-06 13:18:02 -0600

    Refactor Hamming Challenge

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,13 +1,20 @@
+# class Hamming
+#   def self.compute(a, b)
+#     if a.length == b.length
+#       first = a.split('')
+#       second = b.split('')
+#       first.zip(second).inject(0) do |diff, pair| 
+#         pair.uniq.length != 1 ? diff += 1 : diff 
+#       end
+#     else
+#       raise ArgumentError
+#     end
+#   end
+# end
+
 class Hamming
   def self.compute(a, b)
-    if a.length == b.length
-      first = a.split('')
-      second = b.split('')
-      first.zip(second).inject(0) do |diff, pair| 
-        pair.uniq.length != 1 ? diff += 1 : diff 
-      end
-    else
-      raise ArgumentError
-    end
+    raise ArgumentError if a.size != b.size
+    (0...a.size).count { |i| a[i] != b[i] }
   end
 end
