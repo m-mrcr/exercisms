9ae16e14e175955d217a1be5de07f9836f3f933f
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-04-09 13:38:31 -0600

    Refactor Raindrops Challenge

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,8 +1,7 @@
 class Raindrops
     def self.convert(number)
-        output = ""
         hash = {3 => 'Pling', 5 => 'Plang', 7 => 'Plong'}
-        hash.each {|i| output << i[1] if number % i[0] == 0 }
+        output = hash.each_with_object("") {|i, str| str << i[1] if number % i[0] == 0 }
         output == "" ? number.to_s : output
     end
 end
