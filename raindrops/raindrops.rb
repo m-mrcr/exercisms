ce02d57dbed775c61e4c7dcb1f1b473ecb039e02
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-04-12 10:30:34 -0600

    Complete Raindrops Challenge

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,7 +1,7 @@
 class Raindrops
-    def self.convert(number)
-        hash = {3 => 'Pling', 5 => 'Plang', 7 => 'Plong'}
-        output = hash.each_with_object("") {|i, str| str << i[1] if number % i[0] == 0 }
-        output == "" ? number.to_s : output
+    DICTIONARY = {3 => 'Pling', 5 => 'Plang', 7 => 'Plong'}
+    def self.convert(num)
+        output = DICTIONARY.each_with_object("") {|i, str| str << i[1] if num % i[0] == 0 }
+        output == "" ? num.to_s : output 
     end
 end
