a15c49dca253c09927aaee91ffea38a8c78ba0e7
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-04-13 19:36:56 -0600

    Refactor Luhn Challenge Again

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,9 +1,13 @@
 class Luhn
-  def self.valid?(str)
-    return false unless str.match?(/^[\d ]*$/) && str.gsub(/\D/, '').length > 1
-    num = str.delete('^0-9')
+  def self.valid?(num)
+    process(num) if num.match?(/^[0-9 ]*$/) && num.scan(/\d/).count > 1
+  end
+
+  def self.process(num)
+    num = num.gsub(/\s*/, '')
           .chars
           .reverse
+          .each
           .each_with_index
           .reduce(0) do |acc, (val, i)| 
             i.odd? ? acc += double(val.to_i) : acc += val.to_i
@@ -16,3 +20,16 @@ class Luhn
     num > 9 ? num - 9 : num
   end
 end
+
+# class Luhn
+#   def self.valid?(str)
+#     str
+#       .gsub(" ", '')
+#       .tap {|s| return false unless s[/\A\d\d+\Z/]}
+#       .chars
+#       .reverse
+#       .map.with_index {|val, i| i.odd? ? val.to_i * 2 : val.to_i}
+#       .map {|val| val > 9 ? val - 9 : val}
+#       .sum % 10 == 0
+#   end
+# end
