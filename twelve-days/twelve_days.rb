d7a532ffa92f0dd00660144662c7ba7382884f99
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-04-22 17:02:07 -0600

    Refactor Twelve Days Challenge

--- twelve-days/twelve_days.rb
+++ twelve-days/twelve_days.rb
@@ -1,6 +1,7 @@
 class TwelveDays
-   ORDINALS = %w[first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth]
-    GIFTS = ["and a Partridge in a Pear Tree.",
+  INTRO = "On the %s day of Christmas my true love gave to me: "
+  ORDINALS = %w[first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth]
+  GIFTS = ["and a Partridge in a Pear Tree.",
             "two Turtle Doves, ",
             "three French Hens, ",
             "four Calling Birds, ",
@@ -12,25 +13,17 @@ class TwelveDays
             "ten Lords-a-Leaping, ",
             "eleven Pipers Piping, ",
             "twelve Drummers Drumming, "]
-  
-  def self.intro(index)
-    "On the #{index} day of Christmas my true love gave to me: "
-  end
 
   def self.gift(index)
     str = ""
     GIFTS[0..index].reverse.each {|gift| str += gift}
     str.slice!("and ") if str == GIFTS[0] && index == 0
-    str
-  end
-
-  def self.line_break(index)
-    index < 11 ? "\n\n" : "\n" 
+    str += (index < 11 ? "\n\n" : "\n")
   end
 
   def self.song
     str = ""
-    ORDINALS.each_with_index {|day, index| str += (intro(day) + gift(index) + line_break(index))}
+    ORDINALS.each_with_index {|day, i| str += ((INTRO % ORDINALS[i]) + gift(i))}
     return str
   end
 end
