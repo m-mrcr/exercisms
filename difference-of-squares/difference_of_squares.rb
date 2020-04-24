66c3cff4f9d0c7793a644f61b1ea546e3e89f17b
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-04-24 17:45:35 -0600

    Complete Difference of Squares Challenge

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,8 +1,17 @@
-=begin
-Write your code for the 'Difference Of Squares' exercise in this file. Make the tests in
-`difference_of_squares_test.rb` pass.
+class Squares
+  def initialize(num)
+    @num = 0..num
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/difference-of-squares` directory.
-=end
+  def square_of_sum
+    @num.sum ** 2
+  end
 
+  def sum_of_squares
+    @num.sum {|n| n**2}
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
