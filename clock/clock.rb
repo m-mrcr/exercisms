f6d0d14807d3fbbd9dc00786a947e13418b4fb52
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-04-25 15:23:15 -0600

    Complete Clock Challenge

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,12 +1,41 @@
-class Clock
+class Clock     
+  attr_reader :total_minutes
+  
   def initialize(hour: 0, minute: 0)
-    @var = hour * 60 + minute
+    @total_minutes = hour * 60 + minute
   end
 
-  def to_s(hour, minute)
-    t = ((hour*60)+(minute))*60
-    return Time.at(t).utc.strftime("%H:%M")
+  def to_s
+   "%02i:%02i" % [*time]
+  end   
+
+  def time
+    [total_hours, remaining_minutes]
+  end 
+
+  def total_hours
+    (total_minutes / 60) % 24
+  end 
+
+  def remaining_minutes
+    total_minutes % 60
+  end 
+
+  def +(other_clock)
+    apply(:+, other_clock)
+  end 
+
+  def -(other_clock)
+    apply(:-, other_clock)
+  end 
+
+  def apply(original_clock, other_clock)
+    @total_minutes = total_minutes.send(original_clock, other_clock.total_minutes)
+    self
+  end 
+
+  def ==(other_clock)
+   to_s == other_clock.to_s
   end
-    
 end
 
