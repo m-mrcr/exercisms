b254ddfb62b2e46996bc66efb0fd5c70e9a02cc8
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-04-28 10:31:36 -0600

    Complete Sieve Challenge

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -1,8 +1,13 @@
-=begin
-Write your code for the 'Sieve' exercise in this file. Make the tests in
-`sieve_test.rb` pass.
+require 'prime'
 
-To get started with TDD, see the `README.md` file in your
-`ruby/sieve` directory.
-=end
+class Sieve
+  def initialize(num)
+    @num = num
+    @primes = []
+  end
 
+  def primes
+    Prime.each(@num) {|prime| @primes << prime}
+    @primes
+  end
+end
