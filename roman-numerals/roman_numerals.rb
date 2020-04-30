4fc57e2e9f6bfbb48ca4803ae99486b956cf19fe
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-04-30 13:40:58 -0600

    Complete Roman Numeral Challenge

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,7 +1,10 @@
 class Integer
+  letters = %w[ M CM D CD C XC L XL X IX V IV I]
+  values  = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  NUMERALS = letters.zip(values).to_h
+
   def to_roman
-    numerals = %w[I V X L C D M]
     num = self
-    binding.pry
+    NUMERALS.map {|ltr, val| (amt, num) = num.divmod(val); ltr * amt}.join
   end
 end
