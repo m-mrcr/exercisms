3cabcd11a3631f9057ebf71c3b9f4c530a8813d0
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-03 13:10:41 -0600

    Complete Leap Challenge

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,8 +1,9 @@
-=begin
-Write your code for the 'Leap' exercise in this file. Make the tests in
-`leap_test.rb` pass.
+module Year
+  module_function
 
-To get started with TDD, see the `README.md` file in your
-`ruby/leap` directory.
-=end
-
+  def leap? year
+    year % 4 == 0 &&
+    year % 100 != 0 ||
+    year % 400 == 0
+  end
+end
