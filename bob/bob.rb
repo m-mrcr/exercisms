16e3ac1c9171aafd427fde9a91fbe2f83bdb5a3f
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-04 19:52:49 -0600

    Complete Bob Challenge

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,8 +1,29 @@
-=begin
-Write your code for the 'Bob' exercise in this file. Make the tests in
-`bob_test.rb` pass.
+class Bob      
+  def self.hey remark
+    if remark =~ /^[\s]+$/ || remark.empty?
+      return "Fine. Be that way!"
+    elsif remark.count("a-z") == 0 && remark.count("A-Z") > 0 && remark[-1] == "?"
+      return "Calm down, I know what I'm doing!" 
+    elsif remark.count("a-z") == 0 && remark.count("A-Z") > 0 && remark[-1] != "?"
+      return "Whoa, chill out!"
+    elsif remark.delete(" ")[-1] == "?"
+      return "Sure."
+    else
+      return "Whatever." 
+    end
+  end
+end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/bob` directory.
-=end
 
+# Bob answers 'Sure.' if you ask him a question.
+
+# He answers 'Whoa, chill out!' if you yell at him.
+
+# He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
+
+# He says 'Fine. Be that way!' if you address him without actually saying
+# anything.
+
+# He answers 'Whatever.' to anything else.
+
+# Bob's conversational partner is a purist when it comes to written communication and always follows normal rules regarding sentence punctuation in English.
