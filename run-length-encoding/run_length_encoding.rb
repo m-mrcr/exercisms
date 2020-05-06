70d5d5e280db92a30cd0516eab20a58ea826bde9
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-06 16:33:38 -0600

    Complete Run Length Encoding Challenge

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -1,10 +1,27 @@
 class RunLengthEncoding
   def self.encode input
-    step1 = input.split('').map.with_object([]) do | char, obj |
+    pairs = input.split('').map.with_object([]) do | char, obj |
       obj << [0, char] if obj.empty?
       obj.last[1] == char ? obj.last[0] += 1 : obj << [1, char]
     end
-    step1.map {|p| p.delete_at(0) if p[0] == 1}
-    step1.join
+    pairs.map {|p| p.delete_at(0) if p[0] == 1}
+    pairs.join
+  end
+
+  def self.decode input
+    arr = []
+    str = ''
+    input.split('').each do |char|
+      if !char.match(/\A[[:alpha:][:blank:]]+\z/)
+        str += char
+      else
+        arr << str
+        arr << char
+        str = ''
+      end      
+    end
+    arr.each_slice(2).to_a.map do |p|
+      !!(p[0].match(/^(\d)+$/)) ? p[1] * p[0].to_i : p[1]
+    end.join
   end
 end
