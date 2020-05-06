8d30d98abe2cf17e9ceb7368d025cef24aba2dfb
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-05 19:06:31 -0600

    Refactor Half of Run Length Encoding Challenge

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -2,11 +2,7 @@ class RunLengthEncoding
   def self.encode input
     step1 = input.split('').map.with_object([]) do | char, obj |
       obj << [0, char] if obj.empty?
-      if obj.last[1] == char
-        obj.last[0] += 1
-      else
-        obj << [1, char]
-      end
+      obj.last[1] == char ? obj.last[0] += 1 : obj << [1, char]
     end
     step1.map {|p| p.delete_at(0) if p[0] == 1}
     step1.join
