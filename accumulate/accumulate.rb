608a23fc94109fd537b373338b0ef3113883b139
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-07 23:39:13 -0600

    Begin Accumulate Challenge

--- accumulate/accumulate.rb
+++ accumulate/accumulate.rb
@@ -1,8 +1,7 @@
-=begin
-Write your code for the 'Accumulate' exercise in this file. Make the tests in
-`accumulate_test.rb` pass.
+class Array
 
-To get started with TDD, see the `README.md` file in your
-`ruby/accumulate` directory.
-=end
+  def self.accumulate
+    
+  end
 
+end
