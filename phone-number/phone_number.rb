fca4da58f9c11071622008e02a8c69ee99bf4935
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-12 14:56:21 -0600

    Refactor Phone Number Challenge

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,20 +1,13 @@
 class PhoneNumber
   def self.clean number
-    pn = number
-    pn.gsub!("+1",'')
-    pn.slice!(0) if pn.length == 11 && pn[0] == "1"
-    pn.delete!("\s\n()-.")
+    pn = number.gsub(/^\+?1|\D/, '')
     return pn if correctly_formatted(pn)
   end
 
   private
 
   def self.correctly_formatted input
-    n = input.split('').values_at(0, 3)
-    x = input.split('').values_at(1, 2, 4..9)
-    verdict = input.length == 10 &&
-    n.join.scan(/[2-9]/).count == 2 &&
-    x.join.scan(/[0-9]/).count == 8
-    return verdict
+    input.length == 10 &&
+    input.split('').values_at(0, 3).all? {|v| v.to_i > 1}
   end
 end
