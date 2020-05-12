8ebd4454753909371f8b176d65140f8e143a3b9e
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-12 14:20:18 -0600

    Complete Phone Number Challenge

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,8 +1,20 @@
-=begin
-Write your code for the 'Phone Number' exercise in this file. Make the tests in
-`phone_number_test.rb` pass.
+class PhoneNumber
+  def self.clean number
+    pn = number
+    pn.gsub!("+1",'')
+    pn.slice!(0) if pn.length == 11 && pn[0] == "1"
+    pn.delete!("\s\n()-.")
+    return pn if correctly_formatted(pn)
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/phone-number` directory.
-=end
+  private
 
+  def self.correctly_formatted input
+    n = input.split('').values_at(0, 3)
+    x = input.split('').values_at(1, 2, 4..9)
+    verdict = input.length == 10 &&
+    n.join.scan(/[2-9]/).count == 2 &&
+    x.join.scan(/[0-9]/).count == 8
+    return verdict
+  end
+end
