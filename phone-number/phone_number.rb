8e386d725939eba5b39795baa9f72a0cf9ff7d0c
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-12 15:07:34 -0600

    Refactor Phone Number Challenge

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,13 +1,5 @@
 class PhoneNumber
   def self.clean number
-    pn = number.gsub(/^\+?1|\D/, '')
-    return pn if correctly_formatted(pn)
-  end
-
-  private
-
-  def self.correctly_formatted pn
-    pn.length == 10 &&
-    [pn[0], pn[3]].all? {|v| v.to_i > 1}
+    $1 if number.gsub(/\D/, '') =~ /\A1?([2-9]\d{2}[2-9]\d{6})\z/
   end
 end
