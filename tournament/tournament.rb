fb0286d44aad4cca7f40f2872feb865622ad2924
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-15 13:46:13 -0600

    Add Progress for Tournament Challenge

--- tournament/tournament.rb
+++ tournament/tournament.rb
@@ -1,8 +1,75 @@
-=begin
-Write your code for the 'Tournament' exercise in this file. Make the tests in
-`tournament_test.rb` pass.
+class Tournament
+  def self.tally input
+    format(input)
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/tournament` directory.
-=end
+  def self.format input
+    bracket = {}
+    get_games(input).each do |game|
+      binding.pry
+      bracket[:home_team] = {MP: 0, W: 0, D: 0, L: 0, P: 0}
+      bracket[:away_team] = {MP: 0, W: 0, D: 0, L: 0, P: 0}
+      score(game)
+    end
+  end
+  
+  def self.score input
+    binding.pry
+    outcome = input[:outcome]
+    case outcome
+      when win
+        @bracket[:home_team][:MP] += 1
+        @bracket[:home_team][:W] += 1
+        @bracket[:home_team][:P] += 3
 
+        @bracket[:away_team][:MP] += 1
+        @bracket[:away_team][:L] += 1
+      when draw
+        @bracket[:home_team][:MP] += 1
+        @bracket[:home_team][:D] += 1
+        @bracket[:home_team][:P] += 1
+
+        @bracket[:away_team][:MP] += 1
+        @bracket[:away_team][:D] += 1
+        @bracket[:away_team][:P] += 1
+      when loss
+        @bracket[:home_team][:MP] += 1
+        @bracket[:home_team][:D] += 1
+        @bracket[:home_team][:P] += 1
+
+        @bracket[:away_team][:MP] += 1
+        @bracket[:away_team][:D] += 1
+        @bracket[:away_team][:P] += 1
+    end
+  end
+  
+  private
+  
+  def self.get_games input
+    step1 = input.split("\n").map {|game| game.split(';')}
+    step2 = step1.map do |game|
+      {:home_team => game[0], :away_team => game[1], :outcome => game[2]}
+    end
+  end
+  
+end
+
+# input.split(';')[2].include?("win")
+
+# - MP: Matches Played
+# - W: Matches Won
+# - D: Matches Drawn (Tied)
+# - L: Matches Lost
+# - P: Points
+
+# A win earns a team 3 points. A draw earns 1. A loss earns 0.
+
+# The outcome should be ordered by points, descending. In case of a tie, teams are ordered alphabetically.
+
+# The result of the match refers to the first team listed. So this line
+
+# ```text
+# Allegoric Alaskans;Blithering Badgers;win
+# ```
+
+# Means that the Allegoric Alaskans beat the Blithering Badgers.
--- tournament/tournament_test.rb
+++ tournament/tournament_test.rb
@@ -1,10 +1,11 @@
 require 'minitest/autorun'
+require 'pry'
 require_relative 'tournament'
 
 # Common test data version: 1.4.0 ee01fe0
 class TournamentTest < Minitest::Test
   def test_just_the_header_if_no_input
-    # skip
+    skip
     input = <<~INPUT
 
     INPUT
@@ -77,7 +78,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_one_match
-    skip
+    # skip
     input = <<~INPUT
     Allegoric Alaskans;Blithering Badgers;win
     Allegoric Alaskans;Blithering Badgers;win
