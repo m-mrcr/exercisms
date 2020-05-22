240d3383a205f10536254019672025b9fe243f62
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-22 17:13:31 -0600

    Complete Beer Song Challenge

--- beer-song/beer_song.rb
+++ beer-song/beer_song.rb
@@ -1,8 +1,22 @@
-=begin
-Write your code for the 'Beer Song' exercise in this file. Make the tests in
-`beer_song_test.rb` pass.
+class BeerSong
+  def self.recite(start, finish)
+    start.downto(start - finish + 1).map { |i| verse(i) }.join("\n")
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/beer-song` directory.
-=end
-
+  def self.verse(num)
+    case num
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" \
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    when 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" \
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    else
+      "#{num} bottles of beer on the wall, #{num} bottles of beer.\n" \
+      "Take one down and pass it around, #{num - 1} bottles of beer on the wall.\n"
+    end
+  end
+end
