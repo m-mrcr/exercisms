9947873558d0578c01766655bd8d2bb696ae22f4
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-23 17:27:34 -0600

    Refactor Space Age Challenge

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,30 +1,21 @@
 class SpaceAge
-  def initialize seconds
+  def initialize(seconds)
     @seconds = seconds
   end
 
-  def on_earth() (@seconds/earth_year_in_seconds).round(2) end
-  def on_mercury() (on_earth/relative_to[:Mercury]).round(2) end
-  def on_venus() (on_earth/relative_to[:Venus]).round(2) end
-  def on_mars() (on_earth/relative_to[:Mars]).round(2) end
-  def on_jupiter() (on_earth/relative_to[:Jupiter]).round(2) end
-  def on_saturn() (on_earth/relative_to[:Saturn]).round(2) end
-  def on_uranus() (on_earth/relative_to[:Uranus]).round(2) end
-  def on_neptune() (on_earth/relative_to[:Neptune]).round(2) end
-
-  private
-
-  def relative_to
-  { Mercury: 0.2408467,
-    Venus: 0.61519726,
-    Mars: 1.8808158,
-    Jupiter: 11.862615,
-    Saturn: 29.447498,
-    Uranus: 84.016846,
-    Neptune: 164.79132 }
-  end
-
-  def earth_year_in_seconds
-    31557600.0
+  {
+    earth:   1.0,
+    mercury: 0.2408467,
+    venus:   0.61519726,
+    mars:    1.8808158,
+    jupiter: 11.862615,
+    saturn:  29.447498,
+    uranus:  84.016846,
+    neptune: 164.79132
+  }.each do |planet, earth_years|
+    seconds_per_year = 31557600
+    define_method :"on_#{planet}" do
+      @seconds.fdiv(seconds_per_year * earth_years).round(2)
+    end
   end
 end
